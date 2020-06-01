d840d0ec22026af149455cccb7aeab194e9681f3
Edgar-Donk <45669035+Edgar-Donk@users.noreply.github.com>
2020-06-01 16:53:25 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.append(os.path.abspath('../')) # sys.path.insert(0, os.path.abspath('.'))
+import pydata_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -61,8 +61,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme_path = ['pydata_sphinx_theme']
+# html_theme_path = ['pydata_sphinx_theme']
 html_theme = "pydata_sphinx_theme" # "sphinx_rtd_theme" # "sphinxdoc" # 'alabaster'
+html_theme_path = pydata_sphinx_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
